0ec7608d8b0d4c8accc9b734d03ce4d8d0ff6550
Svat-dev <svatoslavgetun@gmail.com>
2026-07-28 05:25:52 +0300

    feat: redis set_cache method done

--- internal/core/repository/redis/cache/repository.go
+++ internal/core/repository/redis/cache/repository.go
@@ -15,6 +15,7 @@ type Repository struct {
 
 type RepositoryMethods interface {
 	GetCache(ctx context.Context, key string) (string, error)
+	SetCache(ctx context.Context, key string, dest any) error
 }
 
 func NewRepository(pool core_redis_pool.Pool, prefix string, expiration time.Duration) *Repository {
