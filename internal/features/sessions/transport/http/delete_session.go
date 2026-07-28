6663bccce652cee8327cc3b2baf4898cf6d18736
Svat-dev <svatoslavgetun@gmail.com>
2026-07-28 18:39:46 +0300

    feat: sessions transport done

---
+++ internal/features/sessions/transport/http/delete_session.go
@@ -0,0 +1,43 @@
+package sessions_transport_http
+
+import (
+	"net/http"
+
+	core_logger "github.com/CascadePro/api-golang-server/internal/core/logger"
+	core_http_request "github.com/CascadePro/api-golang-server/internal/core/transport/http/request"
+	core_http_response "github.com/CascadePro/api-golang-server/internal/core/transport/http/response"
+)
+
+// DeleteSession godoc
+// @Summary     Delete session
+// @Description Delete a session by its ID
+// @Tags        sessions
+// @Produce     json
+// @Param       id path string true "Session ID"
+// @Success     204 "Session deleted"
+// @Failure     400 {object} core_http_response.ErrorResponse "Bad request"
+// @Failure     401 {object} core_http_response.ErrorResponse "Unauthorized"
+// @Failure     404 {object} core_http_response.ErrorResponse "Session not found"
+// @Failure     429 {object} core_http_response.ErrorResponse "Too many requests"
+// @Failure     500 {object} core_http_response.ErrorResponse "Internal server error"
+// @Router      /sessions/{id} [delete]
+func (h *HttpHandler) DeleteSession(rw http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := core_logger.FromContext(ctx)
+	responseHandler := core_http_response.NewResponseHandler(log, rw)
+
+	sessionID, err := core_http_request.GetIDPathValue(r, "id", 24)
+	if err != nil {
+		responseHandler.ErrorResponse(err, "failed to get session id path value")
+
+		return
+	}
+
+	if err := h.sessionsService.DeleteSession(ctx, sessionID); err != nil {
+		responseHandler.ErrorResponse(err, "failed to delete session")
+
+		return
+	}
+
+	responseHandler.NoContentResponse()
+}
---
+++ internal/features/sessions/transport/http/dto/session.go
@@ -0,0 +1,61 @@
+package sessions_http_dto
+
+import (
+	"net"
+	"time"
+
+	"github.com/CascadePro/api-golang-server/internal/core/domain"
+)
+
+type SessionDto struct {
+	ID           string             `json:"id"             example:"831f21c1798a7972fa9cda12dac0"`
+	IP           net.IP             `json:"ip"             example:"0.0.0.0"`
+	CreatedAt    time.Time          `json:"created_at"     example:"2006-01-02T15-04-05.000000"`
+	LastActiveAt time.Time          `json:"last_active_at" example:"2006-01-02T15-04-05.000000"`
+	ExpiresAt    time.Time          `json:"expires_at"     example:"2006-01-02T15-04-05.000000"`
+	Metadata     SessionDtoMetadata `json:"metadata"`
+}
+
+type SessionDtoMetadata struct {
+	Location SessionDtoMetadataLocation `json:"location"`
+	Device   SessionDtoMetadataDevice   `json:"device"`
+}
+
+type SessionDtoMetadataLocation struct {
+	Country string  `json:"country,omitempty" example:"Russia"`
+	City    string  `json:"city,omitempty"    example:"Pskov"`
+	Lat     float64 `json:"lat,omitempty"     example:"0.00000"`
+	Lng     float64 `json:"lng,omitempty"     example:"0.00000"`
+}
+
+type SessionDtoMetadataDevice struct {
+	OS      string `json:"os,omitempty"    example:"Windows 10"`
+	Model   string `json:"model,omitempty" example:"BOM-WXX9"`
+	AppName string `json:"app,omitempty"   example:"Cascade Pro"`
+	Type    string `json:"type,omitempty"  example:"desktop"`
+	Version string `json:"vers,omitempty"  example:"1.0.0"`
+}
+
+func SessionDomainToDTO(session domain.Session) SessionDto {
+	return SessionDto{
+		ID:           session.ID,
+		IP:           session.IP,
+		CreatedAt:    session.CreatedAt,
+		LastActiveAt: session.LastActiveAt,
+		ExpiresAt:    session.CreatedAt.Add(time.Duration(session.ExpirationTime)),
+		Metadata: SessionDtoMetadata{
+			Location: SessionDtoMetadataLocation(session.Metadata.Location),
+			Device:   SessionDtoMetadataDevice(session.Metadata.Device),
+		},
+	}
+}
+
+func SessionDomainsToDTOs(domains []domain.Session) []SessionDto {
+	var sessions []SessionDto
+
+	for _, session := range domains {
+		sessions = append(sessions, SessionDomainToDTO(session))
+	}
+
+	return sessions
+}
