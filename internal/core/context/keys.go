da285473eec4f99f6246927a3c91b944bac26648
Svat-dev <svatoslavgetun@gmail.com>
2026-07-31 01:34:07 +0300

    chore: core/context done

---
+++ internal/core/context/keys.go
@@ -0,0 +1,11 @@
+package core_context
+
+type CtxKey int
+
+var (
+	CtxKeyUserID    = CtxKey(0)
+	CtxKeyUserRole  = CtxKey(1)
+	CtxKeySessionID = CtxKey(2)
+	CtxKeyMimeType  = CtxKey(3)
+	CtxKeyTag       = CtxKey(4)
+)
