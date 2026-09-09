c3799b6bd6750f61a4e682fc794cf4f59bf38716
swuttik <svatoslavgetun@gmail.com>
2026-09-09 18:42:44 +0300

    feat: core/errors integrated into transport/http/response

--- internal/core/transport/http/response/errors.go
+++ internal/core/transport/http/response/errors.go
@@ -3,7 +3,8 @@ package core_http_response
 import "time"
 
 type ErrorResponse struct {
-	Error     string    `json:"error"     example:"full error text"`
-	Message   string    `json:"message"   example:"short human-readable message"`
-	Timestamp time.Time `json:"timestamp" example:"2006-01-02T15-04-05.000000"`
+	Code      string            `json:"code"             example:"invalid_credentials"`
+	Message   string            `json:"message"          example:"short human-readable message"`
+	Fields    map[string]string `json:"fields,omitempty"`
+	Timestamp time.Time         `json:"timestamp"        example:"2006-01-02T15-04-05.000000"`
 }
