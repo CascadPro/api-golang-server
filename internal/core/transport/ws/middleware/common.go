70170667a83217183995fb229375c3a035a8d279
swuttik <svatoslavgetun@gmail.com>
2026-09-20 21:28:08 +0300

    fix: core/websocket panic middleware

--- internal/core/transport/ws/middleware/common.go
+++ internal/core/transport/ws/middleware/common.go
@@ -1,6 +1,8 @@
 package core_ws_middleware
 
 import (
+	"fmt"
+
 	core_context "github.com/CascadePro/api-golang-server/internal/core/context"
 	core_logger "github.com/CascadePro/api-golang-server/internal/core/logger"
 	core_ws_conn "github.com/CascadePro/api-golang-server/internal/core/transport/ws/conn"
@@ -9,15 +11,19 @@ import (
 
 func Panic() Middleware {
 	return func(next Handler) Handler {
-		return func(conn *core_ws_conn.Conn) error {
+		return func(conn *core_ws_conn.Conn) (err error) {
 			ctx := conn.Context()
+
 			log := core_logger.FromContext(ctx)
 			locale := core_context.Locale(ctx)
+
 			handler := core_ws_response.NewHandler(conn, log, locale)
 
 			defer func() {
 				if p := recover(); p != nil {
-					handler.PanicResponse(p, "during handle ws request got unexpected panic")
+					handler.PanicResponse(p, "during handle websocket request got unexpected panic")
+
+					err = fmt.Errorf("websocket handler panic: %v", p)
 				}
 			}()
 
