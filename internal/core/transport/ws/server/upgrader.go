da76d6aaf710109181a635f649bbefe0897ce578
swuttik <svatoslavgetun@gmail.com>
2026-09-21 15:04:36 +0300

    feat: core/websocket rate_limit middleware

--- internal/core/transport/ws/server/upgrader.go
+++ internal/core/transport/ws/server/upgrader.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	core_context "github.com/CascadePro/api-golang-server/internal/core/context"
+	"github.com/CascadePro/api-golang-server/internal/core/domain"
+	core_errors "github.com/CascadePro/api-golang-server/internal/core/errors"
 	core_logger "github.com/CascadePro/api-golang-server/internal/core/logger"
 	core_http_response "github.com/CascadePro/api-golang-server/internal/core/transport/http/response"
 	core_ws_client "github.com/CascadePro/api-golang-server/internal/core/transport/ws/client"
@@ -20,6 +22,12 @@ func (s *Server) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 	responseHandler := core_http_response.NewResponseHandler(logger, locale, rw)
 
+	ip, err := core_context.ClientIP(ctx)
+	if err != nil {
+		responseHandler.ErrorResponse(err, "failed to extract client ip")
+		return
+	}
+
 	conn, err := s.upgrader.Upgrade(rw, r, nil)
 	if err != nil {
 		responseHandler.ErrorResponse(err, "failed to upgrade connection")
@@ -33,6 +41,23 @@ func (s *Server) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	wsConn.SetReadLimit(core_ws_client.MaxMessageSize)
 
 	handler := core_ws_response.NewHandler(wsConn, logger, locale)
+
+	limits, err := s.authLimiter.Allow(wsCtx, domain.RealtimeEventAuthRequest, ip)
+	if err != nil {
+		handler.AuthErrorResponse(err)
+
+		_ = wsConn.Close()
+
+		return
+	}
+
+	if !limits.Allowed {
+		handler.AuthErrorResponse(core_errors.ErrTooManyRequests)
+
+		_ = wsConn.Close()
+
+		return
+	}
 
 	claims, err := s.AuthorizeClient(wsConn)
 	if err != nil {
